5e82b7dcf0b15a52723c34942cfd346def79e3de
Igor <example@gmail.com>
2020-01-31 09:55:33 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict';
+/* let answer = confirm("Are you here?");
+
+console.log(answer); */
+
+// console.log("arr" + " - object");
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
